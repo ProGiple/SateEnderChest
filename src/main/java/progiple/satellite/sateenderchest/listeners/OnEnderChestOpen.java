2965a3e029cbbc896813d7e09fd35848caee5e86
ProGiple <ghosthostgtop@gmail.com>
2025-01-09 23:26:01 +0300

    v1.0.6F1

--- src/main/java/progiple/satellite/sateenderchest/listeners/OnEnderChestOpen.java
+++ src/main/java/progiple/satellite/sateenderchest/listeners/OnEnderChestOpen.java
@@ -15,7 +15,7 @@ public class OnEnderChestOpen implements Listener {
     public void onOpen(InventoryOpenEvent e) {
         if (e.getInventory().getType() == InventoryType.ENDER_CHEST) {
             Player player = (Player) e.getPlayer();
-            player.closeInventory();
+            e.setCancelled(true);
 
             if (!PlayerData.getPlayerDataMap().containsKey(player.getName())) new PlayerData(player.getName());
             MenuManager.openInventory(player, new ECMenu(player, Page.getPageMap().get((byte) 0)));
